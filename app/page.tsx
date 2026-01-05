a222b54e334dcaa18c35f86e4071b358663aa8d5
Ibrahim Hussein <ibrahimhussien227@users.noreply.github.com>
2026-01-05 03:43:10 +0200

    implement coin overview and trending coins ui and functionality using the fetcher utility function

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { Suspense } from "react";
 
-const Home = () => {
-  return <div></div>;
+import CoinOverview from "@/components/home/CoinOverview";
+import TrendingCoins from "@/components/home/TrendingCoins";
+import {
+  CoinOverviewFallback,
+  TrendingCoinsFallback,
+} from "@/components/home/Fallback";
+
+const Home = async () => {
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
+
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
--- components/Header.tsx
+++ components/Header.tsx
@@ -13,35 +13,35 @@ const Header = () => {
       <div className="main-container inner">
         <Link href="/">
           <Image
-            src="/logo.svg"
+            src="/assets/logo.svg"
             alt="Crypto Currency logo"
             width={132}
             height={40}
           />
-          <nav>
-            <Link
-              href="/"
-              className={cn("nav-link", {
-                "is-active": pathName === "/",
-                "is-home": true,
-              })}
-            >
-              Home
-            </Link>
-
-            <p>Search Modal</p>
-
-            <Link
-              href="/coins"
-              className={cn("nav-link", {
-                "is-active": pathName === "/coins",
-                "is-home": true,
-              })}
-            >
-              All Coins
-            </Link>
-          </nav>
         </Link>
+        <nav>
+          <Link
+            href="/"
+            className={cn("nav-link", {
+              "is-active": pathName === "/",
+              "is-home": true,
+            })}
+          >
+            Home
+          </Link>
+
+          <p>Search Modal</p>
+
+          <Link
+            href="/coins"
+            className={cn("nav-link", {
+              "is-active": pathName === "/coins",
+              "is-home": true,
+            })}
+          >
+            All Coins
+          </Link>
+        </nav>
       </div>
     </header>
   );
